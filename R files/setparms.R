5d15effc72e2197a5990986b5adcf4539785f2d7
Gregory Milne <gregorymilne@Gregorys-MBP.lan>
2021-04-01 13:52:54 +0100

    Tidying up parameters script

--- R files/setparms.R
+++ R files/setparms.R
@@ -6,7 +6,6 @@ source("R files/demogdat.R")
 # change of directory for cluster
 # source("demogdat.R")
 
-
 pars  <- list(scale=50, shape= 1, agrps=400, amax=100, log.lambda0 = log(0.05),
               log.lambda1 = log(0), log.gradient = log(0), se=0.98475, sp=0.98725)
 pars$la <- (pars$amax/pars$agrps)  # no. years in each age group (here = 1 yr)
@@ -14,9 +13,6 @@ pars$da <-  1/pars$la  # ageing rate
 pars$age <- seq(0+pars$la/2, pars$amax-pars$la/2, length.out=pars$agrps) # age at midpoints of age groups
 pars$r <- 1/(21/365)  # maternally-derived IgG half life of 3 weeks (Villard et al., 2016. Diagnostic microbiology and infectious disease;84(1):22-33)
 pars$mctr <- c(0.15, 0.44, 0.71)  # SYROCOT 2007. The Lancet 369
- 
-# ## Best fit parameters for Netherlands '95/'96 data:
-# # lambda0 = 2.235830e+01; lambda1 = 7.570937e-04; gradient = 2.957032e-02; shape = 5.647195e-04
  
 # ### interpolated parameters from demographic data
 f <- spline(age_pop, tot_pop, xout=pars$age)
@@ -26,6 +22,7 @@ pars$d <- spline(age_mort, mort, xout=pars$age)$y # interpolated age specific pe
 pars$d[which(pars$d<0)] <- 0  # zero minus elements
 pars$propfert <- approx(age_fert,prop_fert_age,  xout=pars$age)$y
 pars$propfert[is.na(pars$propfert)] <- 0
+pars$propfert <- pars$propfert
 pars$propfert <- pars$propfert/sum(pars$propfert)
 
 #rm(f, age_pop, tot_pop, age_mort, mort, age_fert, births_age, pf)
