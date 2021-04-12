92fe3a79ad12fa348e9b2cf1172147c6fc367992
Gregory Milne <gregorymilne@Gregorys-MBP.lan>
2021-04-12 12:42:47 +0100

    tidying up

--- R files/setparms.R
+++ R files/setparms.R
@@ -2,12 +2,12 @@
 ## SET PARAMETERS & INITs ######
 #############################
 #directory when not using cluster
-source("R files/demogdat.R")
+# source("R files/demogdat.R")
 # change of directory for cluster
-# source("demogdat.R")
+source("demogdat.R")
 
-pars  <- list(scale=50, log.shape= log(0.30), agrps=400, amax=100, log.lambda0 = log(0.02),
-              log.lambda1 = log(0), log.gradient = log(0), se=0.98475, sp=0.98725)
+pars  <- list(scale=50, log.shape= log(0.60), agrps=400, amax=100, log.lambda0 = log(0.02),
+              log.lambda1 = log(0.0001), log.gradient = log(0.20), log.tdecline = log(20), se=0.98475, sp=0.98725)
 pars$la <- (pars$amax/pars$agrps)  # no. years in each age group (here = 1 yr)
 pars$da <-  1/pars$la  # ageing rate
 pars$age <- seq(0+pars$la/2, pars$amax-pars$la/2, length.out=pars$agrps) # age at midpoints of age groups
