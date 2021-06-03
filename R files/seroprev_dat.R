cb972af9116077919053a807b45c9b1e36148684
Gregory Milne <gregorymilne@Gregorys-MBP.lan>
2021-06-03 11:37:57 +0100

    keep all info on immunoassay methods used, for same median sampling years (this info was getting lost before)

--- R files/seroprev_dat.R
+++ R files/seroprev_dat.R
@@ -29,7 +29,7 @@ data <- data %>%
   filter(n() >= min_samples)
 
 # create separate dfs for countries, group by year & calculate weighted prevalence
-countries <- unique(data$country)
+countries <- sort(unique(data$country))
 data_list <- vector(mode = "list", length = length(countries))
 
 for(i in 1:length(countries)){
@@ -42,29 +42,84 @@ for(i in 1:length(countries)){
     summarise(w.prev = weighted.mean(prev, n), k=sum(k), n = sum(n), country=countries[i], 
               state=state, method=method, method2=method2, method3=method3, prop_method1=prop_method1,
               prop_method2=prop_method2, prop_method3=prop_method3) %>%
-    distinct(k, n, .keep_all = T) %>% #remove duplicate rows (where median sampling years are equal)
+    # distinct(k, n, .keep_all = T) %>% #remove duplicate rows (where median sampling years are equal)
     as.data.frame()
 }
 
-# # make ggplots for each of the countries
-# p <- vector(mode = "list", length = length(countries))
-# 
-# #for(j in 1:length(who_regions)){  ## if you want to order plots by region too
-# for(i in 1:length(countries)){
-#   p[[i]] <- ggplot(data_list[[i]], aes(x=year, y=w.prev)) + 
-#     geom_point() + 
-#     geom_smooth(method = "lm", formula = 'y~x') +
-#     xlab("Year") + ylab("Seroprevalence") +
-#     scale_y_continuous(breaks = seq(0,1, 0.2)) +
-#     labs(title = paste(countries[i])) + 
-#     theme_light()
-# }
+## combine methods into one row, for matching median sampling years
+f <- function(x) {
+  x <- na.omit(x)
+  if (length(x) > 0) paste(x,collapse=', ') else NA
+}
 
-# wrap_plots(p)
+res <- vector(mode = "list", length = length(countries))
+
+for(i in 1:length(data_list)){
+  res[[i]] <- data_list[[i]] %>% group_by(year) %>% summarise_all(list(f))
+}
+
+## correct country names to include only one instance in each row
+for(i in 1:length(data_list)){
+  res[[i]]$country <- countries[i]
+}
+
+## correct k to include only one instance in each row
+for(i in 1:length(res)){
+  for(j in 1:nrow(res[[i]])){
+    x <- strsplit(res[[i]]$k, ", ")
+    if(length(x[[j]]) > 1) {
+      res[[i]]$k[j] <- x[[j]][1] #select first mention
+    }
+  }
+}
+
+## correct n to include only one instance in each row
+for(i in 1:length(res)){
+  for(j in 1:nrow(res[[i]])){
+    x <- strsplit(res[[i]]$n, ", ")
+    if(length(x[[j]]) > 1) {
+      res[[i]]$n[j] <- x[[j]][1] #select first mention
+    }
+  }
+}
+
+## correct weighted seroprevalence to include only one instance in each row
+for(i in 1:length(res)){
+  for(j in 1:nrow(res[[i]])){
+    x <- strsplit(res[[i]]$w.prev, ", ")
+    if(length(x[[j]]) > 1) {
+      res[[i]]$w.prev[j] <- x[[j]][1] #select first mention
+    }
+  }
+}
+  
+## make altered columns numeric
+for(i in 1:length(res)){
+  res[[i]]$k <- as.numeric(res[[i]]$k)
+  res[[i]]$n <- as.numeric(res[[i]]$n)
+  res[[i]]$w.prev <- as.numeric(res[[i]]$w.prev)
+}
+  
+
+## make ggplots for each of the countries
+p <- vector(mode = "list", length = length(countries))
+
+# for(j in 1:length(who_regions)){  ## if you want to order plots by region too
+for(i in 1:length(countries)){
+  p[[i]] <- ggplot(res[[i]], aes(x=year, y=w.prev)) +
+    geom_point() +
+    geom_smooth(method = "lm", formula = 'y~x') +
+    xlab("Year") + ylab("Seroprevalence") +
+    scale_y_continuous(breaks = seq(0,1, 0.2)) +
+    labs(title = paste(countries[i])) +
+    theme_light()
+}
+
+wrap_plots(p)
 
 
 ## put data list into one dataframe
-df <- do.call(rbind.data.frame, data_list)
+df <- do.call(rbind.data.frame, res)
 df$country <- as.factor(df$country)
 
 # saveRDS(df, "data/global_data.rds")
