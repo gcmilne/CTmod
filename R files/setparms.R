03dd08d06effda7b98721292b1900d9ecd4277ed
Gregory Milne <gregorymilne@Gregorys-MacBook-Pro.local>
2021-07-22 14:49:59 +0100

    renamed from "ggplots.R" -- supplementary figures

--- R files/setparms.R
+++ R files/setparms.R
@@ -34,7 +34,7 @@ if (cluster == "none"){ #local
 
 countries <- sort(unique(df$country)) #countries in the dataset
 
-pars$country <- "Saudi Arabia"
+pars$country <- "Burkina Faso"
 fitting_data <- subset(df, df$country == pars$country)
 
 # Load demographic data
