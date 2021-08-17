3410a6c45f77d9d293f60fa99d5773f5699d0cfb
Gregory Milne <gmilne@rvc.ac.uk>
2021-08-17 09:17:35 +0100

    Moving functions to a new R file and sourcing  them

--- R files/fitting.R
+++ R files/fitting.R
@@ -41,11 +41,13 @@ if (cluster == "none") { #local
   source("R files/setparms.R")
   source("R files/diagnostics.R")
   source("R files/model.R")
+  source("R files/funcs.R")
   
 } else if (cluster == "RVC" | cluster == "UCL"){ #cluster
   source("setparms.R")
   source("diagnostics.R")
   source("model.R")
+  source("funcs.R")
 }
 
 #################
@@ -76,43 +78,6 @@ npars <- 3
 
 ## Change objects according to dataset being fit:
 matched_indices <- which(pars$propfert !=0)  #child-bearing ages
-
-###############
-## Functions ##
-###############
-## Function to get age profiles at given time
-getit <- function(time) {
-  row <- which(abs(sol[,"time"]-time)==min(abs(sol[,"time"]-time)))
-  df <- sol[row,-1]
-  S  <- df[1:pars$agrps]  
-  I  <- df[(pars$agrps+1):(2*pars$agrps)]  
-  Im <- df[(2*pars$agrps+1):(3*pars$agrps)]
-  pI <- df[(3*pars$agrps+1):(4*pars$agrps)]
-  obs_pI <- df[(4*pars$agrps+1):(5*pars$agrps)]
-  dprev <- df[(5*pars$agrps+1):(6*pars$agrps)]
-  seroconv1 <- df[(6*pars$agrps+1):(7*pars$agrps)] 
-  seroconv2 <- df[(7*pars$agrps+1):(8*pars$agrps)] 
-  seroconv3 <- df[(8*pars$agrps+1):(9*pars$agrps)] 
-  matAb1 <- df[(9*pars$agrps+1):(10*pars$agrps)]
-  matAb2 <- df[(10*pars$agrps+1):(11*pars$agrps)]
-  matAb3 <- df[(11*pars$agrps+1):(12*pars$agrps)]
-  ct1 <- df[(12*pars$agrps+1):(13*pars$agrps)]
-  ct2 <- df[(13*pars$agrps+1):(14*pars$agrps)]
-  ct3 <- df[(14*pars$agrps+1):(15*pars$agrps)]
-  Na <- df[(15*pars$agrps+1):(16*pars$agrps)]
-  age <- pars$age  
-  out <- data.frame(a=age, I=I,Im=Im, pI=pI, obs_pI=obs_pI, dprev=dprev, 
-                    seroconv1=seroconv1, seroconv2=seroconv2, seroconv3=seroconv3, 
-                    matAb1=matAb1, matAb2=matAb2, matAb3=matAb3, 
-                    ct1=ct1, ct2=ct2, ct3=ct3, Na=Na)
-  ## remove last age category for aesthetics
-  # out <- out[-nrow(out),]
-}
-
-## Likelihood function ##
-loglik <- function (k, n, prev) {
-  dbinom(k, n, prev, log = T)
-}
 
 logliks <- vector("numeric", length=nrow(fitting_data))  #initialise log likelihood vector
 
