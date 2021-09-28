e9a60a2fbf2befd76979662cbe4d4cea2f04c6bc
Gregory Milne <gmilne@rvc.ac.uk>
2021-09-28 19:25:57 -0400

    Added support for fonts & made colour scheme consistent across other plots

--- R files/linear_mods.R
+++ R files/linear_mods.R
@@ -5,6 +5,17 @@
 ## Fit a hierarchical mixed effects model for all countries together
 library(lme4)
 library(ggplot2)
+library(RColorBrewer)
+
+#############
+# Set fonts #
+#############
+if(.Platform$OS.type == "windows") { # set Times New Roman font on Windows
+  # library(extrafont)
+  # font_import()
+  # loadfonts(device = "win")
+  windowsFonts(Times=windowsFont("TT Times New Roman")) 
+}
 
 # read in data
 df <- readRDS("data/global_data.rds")
@@ -39,30 +50,30 @@ df$yfit1 <- fitted(m1)
 #CIs around estimate
 confint(m1)
 
-
 # rename Iran to have shorter name for plotting
 df$country <- factor(df$country, labels= c(levels(df$country)[1:6], "Iran", levels(df$country)[8:11]))
 
 #create custom color scale
-library(RColorBrewer)
-# myColors <- brewer.pal(11,"PuOr")
-# myColors[6] <- brewer.pal(11,"PRGn")[5]   #make pale colours bolder
-
-display.brewer.all(n=11,type="div",exact.n=TRUE)
-myColors <- brewer.pal(11,"Spectral")
-
-
+myColors <- brewer.pal(11,"PuOr")
+myColors[6] <- brewer.pal(11,"PRGn")[5]   #make pale colour bolder
 names(myColors) <- levels(df$country)
 colScale <- scale_colour_manual(name = "plotcols",values = myColors)
+
+# Make seroprevalence as %
+df$prev  <- df$prev*100
+df$ci_lo <- df$ci_lo*100
+df$ci_up <- df$ci_up*100
+df$yfit1 <- df$yfit1*100
 
 
 #plot
 p1 <- ggplot(data=df, aes(y=yfit1, x=year, colour = country)) +
   geom_line() +
-  geom_point(data=df, aes(y=prev, x=year)) +
+  geom_point(data=df, aes(y=prev, x=year), size=.8) +
+  geom_errorbar(aes(width=.3, x=year, ymin = ci_lo, ymax = ci_up), size=0.2) +
   facet_wrap(~country, ncol=3) +
   scale_x_continuous(limits=c(1980, 2020), breaks=seq(1983, 2016, 11)) + 
-  ylab("Seroprevalence") +
+  ylab("Seroprevalence (%)") +
   xlab("Median sampling year") +
   theme_light(base_size = 12, base_line_size = 0, base_family = "Times") +
   theme(strip.background = element_rect(fill="white")) +
@@ -75,14 +86,17 @@ p1 <- ggplot(data=df, aes(y=yfit1, x=year, colour = country)) +
 p1 + colScale
 
 #Save
+#PDF
 ggsave(filename = "plots/lm-global-seroprev.pdf",
        device = cairo_pdf, height = 6, width = 6, units = "in")
+
+#PNG
+ggsave(filename = "plots/lm-global-seroprev.png",
+       dpi=600, height = 6, width = 6, units = "in")
 
 ## no evidence to support the more complex model
 anova(m0, m1)
 
-
 ## estimate accuracy of publication year as proxy for sampling year
-
 mean(df$year_published) - mean(df$year)
 t.test(df$year_published, df$year, paired=T)
