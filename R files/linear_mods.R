9490a880549207be3c23ed5af316e870c7bd4f5e
Gregory Milne <gmilne@rvc.ac.uk>
2021-09-29 12:41:00 -0400

    Make code clearer to read

--- R files/linear_mods.R
+++ R files/linear_mods.R
@@ -1,8 +1,8 @@
-############
-## Linear models
-############
+############################################################################
+## Linear models & plotting of relationship between seroprevalence & time ##
+############################################################################
 
-## Fit a hierarchical mixed effects model for all countries together
+## Fit a hierarchical mixed effects model
 library(lme4)
 library(ggplot2)
 library(RColorBrewer)
@@ -17,7 +17,9 @@ if(.Platform$OS.type == "windows") { # set Times New Roman font on Windows
   windowsFonts(Times=windowsFont("TT Times New Roman")) 
 }
 
-# read in data
+###############
+## Load data ##
+###############
 df <- readRDS("data/global_data.rds")
 
 # calculate prevalence
@@ -26,47 +28,41 @@ df$prev <- df$k/df$n
 # standardise year
 df$yrstd <- (df$year-mean(df$year))/sd(df$year)  #intercept where year is avg year rather than =0
 
-## over-fitted model
+## complex model (country as random intercept + random gradient)
 m0 <- lmer(prev~yrstd+(yrstd|country), data=df)
 
-df$yfit0 <- fitted(m0)
+## simple model (country as random intercept but not random gradient)
+m1<-lmer(prev~yrstd+(1|country), data=df)
 
-p0 <- ggplot(data=df, aes(y=yfit0, x=year, group=country, col=country)) +
-  geom_line(show.legend = F) +
-  geom_point(data=df, aes(y=prev, x=year, col=country), show.legend = F) +
-  facet_wrap(~country) + 
-  ylab("Seroprevalence") + 
-  xlab("Year") + 
-  theme_light()
+## compare complex vs. simple model
+anova(m0, m1) #no evidence to support the more complex model
 
-## simpler model
-# m1<-lmer(prev~yrstd+(1|country), data=df)
+## estimate accuracy of publication year as proxy for sampling year
+mean(df$year_published) - mean(df$year)
+t.test(df$year_published, df$year, paired=T)
 
-## estimate annual declines in seroprevalence
-# model with un-standardised year
-m1<-lmer(prev~year+(1|country), data=df)
+## estimate annual declines in seroprevalence (model with un-standardised year)
+m1       <- lmer(prev~year+(1|country), data=df)
 df$yfit1 <- fitted(m1)
+confint(m1)  #95% CIs around estimate
 
-#CIs around estimate
-confint(m1)
-
+## Plot fit of simpler linear model to the data ##
 # rename Iran to have shorter name for plotting
 df$country <- factor(df$country, labels= c(levels(df$country)[1:6], "Iran", levels(df$country)[8:11]))
 
-#create custom color scale
-myColors <- brewer.pal(11,"PuOr")
-myColors[6] <- brewer.pal(11,"PRGn")[5]   #make pale colour bolder
+# create custom color scale
+myColors        <- brewer.pal(11,"PuOr")
+myColors[6]     <- brewer.pal(11,"PRGn")[5]   #make pale colour bolder
 names(myColors) <- levels(df$country)
-colScale <- scale_colour_manual(name = "plotcols",values = myColors)
+colScale        <- scale_colour_manual(name = "plotcols",values = myColors)
 
-# Make seroprevalence as %
+# make seroprevalence as %
 df$prev  <- df$prev*100
 df$ci_lo <- df$ci_lo*100
 df$ci_up <- df$ci_up*100
 df$yfit1 <- df$yfit1*100
 
-
-#plot
+# plot
 p1 <- ggplot(data=df, aes(y=yfit1, x=year, colour = country)) +
   geom_line() +
   geom_point(data=df, aes(y=prev, x=year), size=.8) +
@@ -78,9 +74,7 @@ p1 <- ggplot(data=df, aes(y=yfit1, x=year, colour = country)) +
   theme_light(base_size = 12, base_line_size = 0, base_family = "Times") +
   theme(strip.background = element_rect(fill="white")) +
   theme(strip.text = element_text(colour = 'black')) +
-  theme(legend.position = "none", panel.grid = element_blank(), axis.ticks = element_blank()#, 
-        # strip.background = element_blank()
-        )
+  theme(legend.position = "none", panel.grid = element_blank(), axis.ticks = element_blank())
 
 #add colours
 p1 + colScale
@@ -93,10 +87,3 @@ ggsave(filename = "plots/lm-global-seroprev.pdf",
 #PNG
 ggsave(filename = "plots/lm-global-seroprev.png",
        dpi=600, height = 6, width = 6, units = "in")
-
-## no evidence to support the more complex model
-anova(m0, m1)
-
-## estimate accuracy of publication year as proxy for sampling year
-mean(df$year_published) - mean(df$year)
-t.test(df$year_published, df$year, paired=T)
