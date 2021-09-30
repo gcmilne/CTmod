a394f5ded419685e750a6b5388e10a869fe8abf2
Gregory Milne <gmilne@rvc.ac.uk>
2021-09-30 12:34:37 -0400

    colour ggplots by socioeconomic status & move temporal CT calculations to another script

--- R files/plot_postpred.R
+++ R files/plot_postpred.R
@@ -12,7 +12,6 @@ library(RColorBrewer)
 library(purrr)
 library(RColorBrewer)
 
-
 #############
 # Load data #
 #############
@@ -30,11 +29,22 @@ if(.Platform$OS.type == "windows") { # set Times New Roman font on Windows
   windowsFonts(Times=windowsFont("TT Times New Roman")) 
 }
 
+
+######################################
+## Set country socioeconomic status ##
+######################################
+# Below from World Bank (https://data.worldbank.org/country)
+income_status <- list("high" = c("Italy", "Saudi Arabia", "United Kingdom"), 
+                      "upper_middle" = c("Brazil", "China", "Turkey"),
+                      "lower_middle" = c("Cameroon", "India", "Iran"),
+                      "low" = c("Burkina Faso", "Ethiopia"))
+
+
 ################
 ## Make plots ##
 ################
 min_year <- 1980 #Minimum year to display modelled estimates from
-max_year <- 2030 #Maximum year to display modelled estimates from
+max_year <- 2030 #Maximum year to display modelled estimates until
 
 ## Prevalence ##
 
@@ -44,15 +54,27 @@ if (!exists("prev_allyears")) {  #only create if list not in existence
 }
 
 #Colour for geom_ribbon
-# display.brewer.pal(8, "Pastel1")
+ribbonColour <- brewer.pal(8, "Pastel2")[c(3, 7)]
+
+#Colours for income status
+income_cols <- brewer.pal(4, "Blues")
 
 levels(countries)[7] <- "Iran" ## Make Iran's name shorter for plotting
 
 for (i in 1:length(countries)) {
   
-  pars$country <- levels(countries)[i]
+  pars$country <- levels(countries)[i]  #set country
   
-  ribbonColour <- brewer.pal(8, "Pastel2")[c(3, 7)]
+  ## store title colour according to income status
+  if (pars$country %in% income_status$high) {
+    ses_colour <- income_cols[1]
+  } else if (pars$country %in% income_status$upper_middle) {
+    ses_colour <- income_cols[2]
+  } else if (pars$country %in% income_status$lower_middle) {
+    ses_colour <- income_cols[3]
+  } else if (pars$country %in% income_status$low) {
+    ses_colour <- income_cols[4]
+  }
   
   prev_allyears[[i]] <- ggplot(
     data=prev_all[[i]]) + 
@@ -63,8 +85,6 @@ for (i in 1:length(countries)) {
                aes(x=time, y=dat_prev), col="grey", size=0.2) +
     geom_errorbar(data=prev_fit[[i]], 
                   aes(width=.3, x=time, ymin = dat_low, ymax = dat_up),  col="grey", size=0.2) +
-    # annotate("rect", xmin=min(fitting_data$year), xmax=max(fitting_data$year), #years with data
-    #          ymin=0, ymax=Inf, alpha=0.3, fill=ribbonColour[1]) +
     xlab("Year") + 
     ylab("Seroprevalence (%)") + 
     scale_x_continuous(expand = c(0,0), limits = c(min_year, max_year), breaks = c(min_year, 2005, max_year)) + 
@@ -75,6 +95,11 @@ for (i in 1:length(countries)) {
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + 
     theme(plot.margin=grid::unit(c(0, 0.5, 0, 0),"cm"))
   
+  prev_allyears[[i]] <- prev_allyears[[i]] + 
+    theme(panel.background = element_rect(colour = ses_colour, 
+                                          size = 2.5, linetype = "solid"))
+  
+  
 }
 
 ## Plot & save multipanel prevalence plot
@@ -83,7 +108,7 @@ wrap_plots(prev_allyears, nrow=4, ncol=3) +
     tag_levels = list(c('(a)', '(b)', '(c)', '(d)', '(e)', '(f)', 
                         '(g)', '(h)', '(i)', '(j)', '(k)')))
 
-ggsave(filename = "plots/prev_multipanel.pdf",
+ggsave(filename = "plots/prev_multipanel_test.pdf",
        device = cairo_pdf, height = 8, width = 8, units = "in")
 
 # ggsave(filename = "plots/ct_sequelae_multipanel.png",
@@ -234,140 +259,3 @@ wrap_plots(ct_combo, nrow=4, ncol=3) &
 
  # ggsave(filename = "plots/ct_sequelae_multipanel.png",
  #        height = 8, width = 8, units = "in", dpi=600)
-
-
-#####################################
-## Temporal change in CT incidence ##
-#####################################
-if (!exists("ct_change")) {  #only create if list not in existence
-  ct_change <- vector("list", length=length(countries))
-}
-
-min_index <- max_index <- vector("list", length=length(countries))
-
-# Create dataset with just past & present casting (excluding forecasts)
-ct_past <- vector("list", length=length(countries))
-
-for (i in 1:length(ct_past)) {
-  ct_past[[i]]$time   <- ct_all[[i]]$time      [ which(ct_all[[i]]$time == 1900) : which(ct_all[[i]]$time == 2021)]
-  ct_past[[i]]$ct     <- ct_all[[i]]$ct_rel    [ which(ct_all[[i]]$time == 1900) : which(ct_all[[i]]$time == 2021)]
-  ct_past[[i]]$ct_low <- ct_all[[i]]$ct_rel_low[ which(ct_all[[i]]$time == 1900) : which(ct_all[[i]]$time == 2021)]
-  ct_past[[i]]$ct_up  <- ct_all[[i]]$ct_rel_up [ which(ct_all[[i]]$time == 1900) : which(ct_all[[i]]$time == 2021)]
-}
-
-# Calculate min and max incidence for all countries
-for(i in 1:length(countries)){
-  ct_change[[i]]$baseline <- ct_past[[i]]$ct[1]  #incidence at year 1900
-  ct_change[[i]]$max_ct   <- max(ct_past[[i]]$ct)#max incidence
-  ct_change[[i]]$min_ct   <- min(ct_past[[i]]$ct)#min incidence
-  
-  min_index <- which(ct_past[[i]]$ct == ct_change[[i]]$min_ct) #find min index 
-  ct_change[[i]]$min_ct_low <- ct_past[[i]]$ct_low[min_index]    #lower CI
-  ct_change[[i]]$min_ct_up  <- ct_past[[i]]$ct_up[min_index]     #upper CI
-  
-  max_index <- which(ct_past[[i]]$ct == ct_change[[i]]$max_ct) #find max index 
-  ct_change[[i]]$max_ct_low <- ct_past[[i]]$ct_low[max_index]    #lower CI
-  ct_change[[i]]$max_ct_up  <- ct_past[[i]]$ct_up[max_index]     #upper CI
-}
-
-# Print ranges of incidence predictions
-for(i in 1:length(countries)){
-  print(paste(
-    countries[i], ": ", round(ct_change[[i]]$min_ct, 1), 
-    " (", round(ct_change[[i]]$min_ct_low, 1), ", ", round(ct_change[[i]]$min_ct_up, 1), ") ", 
-    "– ", round(ct_change[[i]]$max_ct, 1),
-    " (", round(ct_change[[i]]$max_ct_low, 1), ", ", round(ct_change[[i]]$max_ct_up, 1), ") ", 
-    sep=""
-    
-  ))
-}
-
-
-
-# which countries have increasing CT?
-for(i in 1:length(countries)){
-  
-  if (ct_change[[i]]$max_ct > ct_change[[i]]$baseline) {
-    
-    print(paste("Country: ", countries[i], "; ", 
-                # "Baseline CT incidence: ", round(ct_change[[i]]$baseline, 2), 
-                # "Max CT incidence: ", round(ct_change[[i]]$max_ct, 2), 
-                "Fold-change=", round(ct_change[[i]]$max_ct / ct_change[[i]]$baseline, 1), 
-                sep = ""))
-    
-  }
-}
-
-
-for (i in 1:length(countries)) {
-  
-  #if minimum CT incidence is at baseline
-  if (ct_change[[i]]$baseline == ct_change[[i]]$min_ct) { 
-    
-    #calculate median percentage increase in incidence
-    ct_change[[i]]$percent_change <- round ( 
-      (ct_change[[i]]$max_ct / ct_change[[i]]$baseline) * 100, 2
-    )
-    
-    # else if minimum CT incidence is post-baseline
-  } else if (ct_change[[i]]$baseline != ct_change[[i]]$min_ct) {
-    
-    #calculate median percentage decrease in incidence
-    ct_change[[i]]$percent_change <- round ( 
-      (ct_change[[i]]$max_ct / ct_change[[i]]$baseline) * 100, 2
-    )
-    
-  }
-  
-  
-}
-
-
-# Extract CT estimates for specific years
-
-# Min:max sampling years that systematic reivew extracted data (Rostami et al 2019 PloS NTD)
-years_list <- list(1997:2014, 
-                   2004:2014, 
-                   2009, 
-                   1993:2017, 
-                   2011:2015, 
-                   2002:2017, 
-                   1999:2017, 
-                   1987:2014, 
-                   1999:2016, 
-                   2002:2016,
-                   1992)
-
-# Create lists to store indices & matched CT estimates
-matched_ct <- indices <- vector("list", length=length(years_list))
-
-# Fill lists 
-for (i in 1:length(years_list)) {
-  
-  # Find indices
-  indices[[i]] <- which(ct_all[[i]]$time == years_list[[i]][1]): which(ct_all[[i]]$time == years_list[[i]][length(years_list[[i]])])
-  
-  
-  # Find CT incidence & uncertainties
-  matched_ct[[i]]$ct      <- ct_all[[i]]$ct_rel    [indices[[i]]]
-  matched_ct[[i]]$ct_low  <- ct_all[[i]]$ct_rel_low[indices[[i]]]
-  matched_ct[[i]]$ct_high <- ct_all[[i]]$ct_rel_up [indices[[i]]]
-  
-}
-
-# Calculate median CT incidence over whole period
-point_estimates <- vector("list", length=length(matched_ct))
-
-for (i in 1:length(point_estimates)) {
-  
-  point_estimates[[i]]$central <- round(median(matched_ct[[i]]$ct), 1)
-  point_estimates[[i]]$low     <- round(median(matched_ct[[i]]$ct_low), 1)
-  point_estimates[[i]]$up      <- round(median(matched_ct[[i]]$ct_high), 1)
-  
-}
-
-# Display in useful format for table
-for (i in 1:length(point_estimates)) {
-  print(paste(countries[i], ": ", point_estimates[[i]]$central, " (", 
-              point_estimates[[i]]$low, ", ", point_estimates[[i]]$up, ")", sep=""))
-}
