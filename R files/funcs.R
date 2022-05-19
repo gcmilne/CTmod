542cb7141bb9d6a775bfba9bea47d04b3f0468ce
Gregory Milne <gmilne@rvc.ac.uk>
2022-05-19 11:00:47 +0100

    make getit function toggleable by model age structure

--- R files/funcs.R
+++ R files/funcs.R
@@ -3,52 +3,57 @@
 ###############
 
 ## Function to get age profiles at given time (for 3-month age windows)
-getit <- function(time) {
-  row <- which(abs(sol[,"time"]-time)==min(abs(sol[,"time"]-time)))
-  df <- sol[row,-1]
-  S         <- df[1:pars$agrps]  
-  I         <- df[(pars$agrps+1):(2*pars$agrps)]  
-  Im        <- df[(2*pars$agrps+1):(3*pars$agrps)]
-  pI        <- df[(3*pars$agrps+1):(4*pars$agrps)]
-  dprev     <- df[(4*pars$agrps+1):(5*pars$agrps)]
-  seroconv1 <- df[(5*pars$agrps+1):(6*pars$agrps)] 
-  seroconv2 <- df[(6*pars$agrps+1):(7*pars$agrps)] 
-  seroconv3 <- df[(7*pars$agrps+1):(8*pars$agrps)] 
-  matAb1    <- df[(8*pars$agrps+1):(9*pars$agrps)]
-  matAb2    <- df[(9*pars$agrps+1):(10*pars$agrps)]
-  matAb3    <- df[(10*pars$agrps+1):(11*pars$agrps)]
-  ct1       <- df[(11*pars$agrps+1):(12*pars$agrps)]
-  ct2       <- df[(12*pars$agrps+1):(13*pars$agrps)]
-  ct3       <- df[(13*pars$agrps+1):(14*pars$agrps)]
-  Na        <- df[(14*pars$agrps+1):(15*pars$agrps)]
-  age       <- pars$age  
-  out <- data.frame(a=age, I=I,Im=Im, pI=pI, dprev=dprev, 
-                    seroconv1=seroconv1, seroconv2=seroconv2, seroconv3=seroconv3, 
-                    matAb1=matAb1, matAb2=matAb2, matAb3=matAb3, 
-                    ct1=ct1, ct2=ct2, ct3=ct3, Na=Na)
-  ## remove last age category for aesthetics
-  # out <- out[-nrow(out),]
-}
-
-
-## Function to get age profiles at given time (for 9-month age windows)
-getit9 <- function(time) {
-  row <- which(abs(sol[,"time"]-time)==min(abs(sol[,"time"]-time)))
-  df  <- sol[row,-1]
-  S        <- df[1:pars$agrps]  
-  I        <- df[(pars$agrps+1):(2*pars$agrps)]  
-  Im       <- df[(2*pars$agrps+1):(3*pars$agrps)]
-  pI       <- df[(3*pars$agrps+1):(4*pars$agrps)]
-  dprev    <- df[(4*pars$agrps+1):(5*pars$agrps)]
-  seroconv <- df[(5*pars$agrps+1):(6*pars$agrps)] 
-  matAb    <- df[(6*pars$agrps+1):(7*pars$agrps)]
-  ct       <- df[(7*pars$agrps+1):(8*pars$agrps)]
-  Na       <- df[(8*pars$agrps+1):(9*pars$agrps)]
-  age      <- pars$age  
-  out <- data.frame(a=age, I=I,Im=Im, pI=pI, dprev=dprev, 
-                    seroconv=seroconv, matAb=matAb, ct=ct, Na=Na)
-  ## remove last age category for aesthetics
-  # out <- out[-nrow(out),]
+if (pars$grps_per_year == 4) {
+  
+  getit <- function(time) {
+    row <- which(abs(sol[,"time"]-time)==min(abs(sol[,"time"]-time)))
+    df <- sol[row,-1]
+    S         <- df[1:pars$agrps]  
+    I         <- df[(pars$agrps+1):(2*pars$agrps)]  
+    Im        <- df[(2*pars$agrps+1):(3*pars$agrps)]
+    pI        <- df[(3*pars$agrps+1):(4*pars$agrps)]
+    dprev     <- df[(4*pars$agrps+1):(5*pars$agrps)]
+    seroconv1 <- df[(5*pars$agrps+1):(6*pars$agrps)] 
+    seroconv2 <- df[(6*pars$agrps+1):(7*pars$agrps)] 
+    seroconv3 <- df[(7*pars$agrps+1):(8*pars$agrps)] 
+    matAb1    <- df[(8*pars$agrps+1):(9*pars$agrps)]
+    matAb2    <- df[(9*pars$agrps+1):(10*pars$agrps)]
+    matAb3    <- df[(10*pars$agrps+1):(11*pars$agrps)]
+    ct1       <- df[(11*pars$agrps+1):(12*pars$agrps)]
+    ct2       <- df[(12*pars$agrps+1):(13*pars$agrps)]
+    ct3       <- df[(13*pars$agrps+1):(14*pars$agrps)]
+    Na        <- df[(14*pars$agrps+1):(15*pars$agrps)]
+    age       <- pars$age  
+    out <- data.frame(a=age, I=I,Im=Im, pI=pI, dprev=dprev, 
+                      seroconv1=seroconv1, seroconv2=seroconv2, seroconv3=seroconv3, 
+                      matAb1=matAb1, matAb2=matAb2, matAb3=matAb3, 
+                      ct1=ct1, ct2=ct2, ct3=ct3, Na=Na)
+    ## remove last age category for aesthetics
+    # out <- out[-nrow(out),]
+  }
+  
+} else if (pars$grps_per_year == 12/9) {
+  
+  ## Function to get age profiles at given time (for 9-month age windows)
+  getit <- function(time) {
+    row <- which(abs(sol[,"time"]-time)==min(abs(sol[,"time"]-time)))
+    df  <- sol[row,-1]
+    S        <- df[1:pars$agrps]  
+    I        <- df[(pars$agrps+1):(2*pars$agrps)]  
+    Im       <- df[(2*pars$agrps+1):(3*pars$agrps)]
+    pI       <- df[(3*pars$agrps+1):(4*pars$agrps)]
+    dprev    <- df[(4*pars$agrps+1):(5*pars$agrps)]
+    seroconv <- df[(5*pars$agrps+1):(6*pars$agrps)] 
+    matAb    <- df[(6*pars$agrps+1):(7*pars$agrps)]
+    ct       <- df[(7*pars$agrps+1):(8*pars$agrps)]
+    Na       <- df[(8*pars$agrps+1):(9*pars$agrps)]
+    age      <- pars$age  
+    out <- data.frame(a=age, I=I,Im=Im, pI=pI, dprev=dprev, 
+                      seroconv=seroconv, matAb=matAb, ct=ct, Na=Na)
+    ## remove last age category for aesthetics
+    # out <- out[-nrow(out),]
+  }
+  
 }
 
 
